ad75c06e213a0dcd4a8e7d430a1d8d303b288959
earth2travis <travis.mccutcheon@gmail.com>
2020-11-07 12:42:47 -0600

    Update background when user scrolls

--- src/components/header.js
+++ src/components/header.js
@@ -2,22 +2,50 @@ import React from "react"
 import { Link } from "gatsby"
 import "./Header.css"
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img
-          src={require("../../src/images/logo-designcode.svg")}
-          width="30"
-          alt=""
-        />
-      </Link>
-      <Link to="#">Technology</Link>
-      <Link to="#">Research</Link>
-      <Link to="#">Company</Link>
-      <button>Pre-Order</button>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}
+      >
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img
+              src={require("../../src/images/logo-designcode.svg")}
+              width="30"
+              alt=""
+            />
+          </Link>
+          <Link to="#">Technology</Link>
+          <Link to="#">Research</Link>
+          <Link to="#">Company</Link>
+          <button>Pre-Order</button>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
